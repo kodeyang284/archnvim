bbb865d7bfd0b6401f99bd0915be4de1abd21e3f
kodeyang <1468466525@qq.com>
2024-12-14 15:34:20 +0800

    merge some configs

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -10,4 +10,13 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.pairs",
+    opts = {
+      mappings = {
+        ["'"] = { action = "close", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+        ["`"] = { action = "close", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+      },
+    },
+  },
 }
